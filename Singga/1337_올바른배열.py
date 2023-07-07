6e801ea030963f4cad82faef2497fc530dd60e7d
singga <qwerewqaa@naver.com>
2023-07-07 18:20:26 +0900

    1337 commit

--- Singga/1337_올바른배열.py
+++ Singga/1337_올바른배열.py
@@ -1,1 +1,20 @@
-print("Hello")
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = []
+for _ in range(n):
+    input = int(sys.stdin.readline())
+    arr.append(input)
+    
+arr.sort()
+
+result = []
+for i in arr:
+    cnt = 0
+    for j in range(5):
+        if (i + j) not in arr:
+            cnt += 1
+    result.append(cnt)
+    
+print(min(result))
